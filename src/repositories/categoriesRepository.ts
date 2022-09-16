6271006b652c67ecd08a31ddc6715db3fbf8e3ce
GB-Navarro <gbnavarro@usp.br>
2022-09-16 15:00:13 -0300

    feat: created the getIdByName at the categoriesRepository

---
+++ src/repositories/categoriesRepository.ts
@@ -0,0 +1,21 @@
+import { client } from "../dbStrategy/postgres.js";
+ 
+async function getIdByName(categoryName: string){
+    const result = await client.categories.findFirst({
+        where:{
+            name: categoryName
+        },
+        select:{
+            id: true
+        }
+    })
+
+    return result;
+}
+
+const categoriesRepository = {
+
+    getIdByName
+}
+
+export default categoriesRepository;
