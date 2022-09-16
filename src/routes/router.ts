067b1893137b3720685924c6b186901b602efd83
GB-Navarro <gbnavarro@usp.br>
2022-09-16 12:33:07 -0300

    feat: added the authRouter to the router

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import exampleRouter from "./exampleRoutes.js";
+import authRouter from "./authRoutes.js";
 
 const router = Router();
 
 router.use(exampleRouter);
+router.use(authRouter);
 
 export default router;
