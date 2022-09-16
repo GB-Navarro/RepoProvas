a9b724d3c9c43d0b472d448bc2e0f99ee9b7b8d7
GB-Navarro <gbnavarro@usp.br>
2022-09-16 15:15:10 -0300

    feat: the insert function at the testServices has been concluded

--- src/services/testServices.ts
+++ src/services/testServices.ts
@@ -1,17 +1,26 @@
 import { ITestData } from "../interfaces/testInterfaces.js";
+import { tests as Tests } from "@prisma/client";
 
-async function insert(data: ITestData){
+import testsRepository from "../repositories/testsRepository.js";
+import teachersRepository from "../repositories/teachersRepository.js";
+import categoriesRepository from "../repositories/categoriesRepository.js";
+import disciplinesRepository from "../repositories/disciplinesRepository.js";
+import teachersDisciplinesRepository from "../repositories/teachersDisciplinesRepository.js";
 
-    const { name, pdfUrl, category, discipline, teacher}: ITestData = data;
+import testUtils from "../utils/testUtils.js";
 
-    //const categoryId: number =  await testRepository.getCategoryIdByName(category);
-    //const teacherId: number =  await testRepository.getTeacherIdByName(teacher);
-    //const disciplineId: number =  await testRepository.getDisciplineIdByName(discipline);
-    //const teacherDisciplineId: number =  await testRepository.getTeacherDisciplineId(teacherId, disciplineId);
+async function insert(data: ITestData) {
 
-    //const insertData: Tests = testUtils.generateInsertData(name, pdfUrl, categoryId, teacherDisciplineId);
+    const { name, pdfUrl, category: categoryName, discipline, teacher }: ITestData = data;
 
-    //await testRepository.insert(inserData);
+    const { id: categoryId }: { id: number } = await categoriesRepository.getIdByName(categoryName);
+    const { id: teacherId }: { id: number } = await teachersRepository.getIdByName(teacher);
+    const { id: disciplineId }: { id: number } = await disciplinesRepository.getIdByName(discipline);
+    const { id: teacherDisciplineId }: { id: number } = await teachersDisciplinesRepository.getIdByTeacherIdAndDisciplineId(teacherId, disciplineId);
+
+    const insertData: Omit<Tests, "id"> = testUtils.generateInsertData(name, pdfUrl, categoryId, teacherDisciplineId);
+
+    await testsRepository.insert(insertData);
 }
 
 const testServices = {
