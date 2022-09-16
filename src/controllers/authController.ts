84bb86b4b5be076564d13d048d2798ea550e1eef
GB-Navarro <gbnavarro@usp.br>
2022-09-16 12:27:51 -0300

    feat: the signUp controller has been concluded

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express";
-import { ISignUpData, IUserData } from "../interfaces/AuthInterfaces.js";
+import { ISignUpData } from "../interfaces/AuthInterfaces.js";
+
+import authServices from "../services/authServices.js";
 
 async function signUp(req: Request, res: Response) {
 
     const data: ISignUpData = req.body;
+
+    await authServices.createUser(data);
 
     res.status(200).send("The user was created!");
 }
