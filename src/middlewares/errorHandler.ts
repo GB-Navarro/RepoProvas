7e968440574942a9b4366a99e1465771e353cc81
GB-Navarro <gbnavarro@usp.br>
2022-09-16 13:07:00 -0300

    fix: fixed some issues involving the .env and errorHandler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -2,6 +2,9 @@ import { Request, Response, NextFunction } from "express";
 
 export default async function errorHandler(error: any, req: Request, res: Response, next: NextFunction){
 
+    if(error.code === "error_thisEmailIsNotRegistered" || error.code === "error_wrongPassword"){
+        return res.status(401).send(error.message);
+    }
     if(error.code === "error_emailAlreadyInUse"){
         return res.status(409).send(error.message);
     }
@@ -9,5 +12,6 @@ export default async function errorHandler(error: any, req: Request, res: Respon
         return res.status(422).send(error.message);
     }
 
+    console.log(error.message);
     return res.sendStatus(500);
 }
