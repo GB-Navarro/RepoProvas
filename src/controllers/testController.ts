f9e75a82a1d103e323bc2dd9852f2406ccca9101
GB-Navarro <gbnavarro@usp.br>
2022-09-16 14:52:50 -0300

    feat: the insert function has been designed

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
+import { ITestData } from "../interfaces/testInterfaces.js";
 
 async function insert(req: Request, res: Response) {
 
+    const data: ITestData = req.body;
+     
     res.status(200).send("Hello World");
 }
 
