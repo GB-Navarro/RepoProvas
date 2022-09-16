192cb17b2337952918a3e6481b899d3f47996a87
GB-Navarro <gbnavarro@usp.br>
2022-09-16 15:15:39 -0300

    feat: the insert function at the testController has been concluded

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
 import { ITestData } from "../interfaces/testInterfaces.js";
+import testServices from "../services/testServices.js";
 
 async function insert(req: Request, res: Response) {
 
     const data: ITestData = req.body;
-     
+    
+    await testServices.insert(data);
+
     res.status(200).send("Hello World");
 }
 
