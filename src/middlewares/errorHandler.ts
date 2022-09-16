c66f5ae4aa7644ceab8775031257d62f73af212c
GB-Navarro <gbnavarro@usp.br>
2022-09-16 12:40:03 -0300

    fix: fixed a issue involving the errorHandler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -2,5 +2,12 @@ import { Request, Response, NextFunction } from "express";
 
 export default async function errorHandler(error: any, req: Request, res: Response, next: NextFunction){
 
+    if(error.code === "error_emailAlreadyInUse"){
+        return res.status(409).send(error.message);
+    }
+    if(error.code === "error_thePasswordsAreNotEqual"){
+        return res.status(422).send(error.message);
+    }
+
     return res.sendStatus(500);
 }
