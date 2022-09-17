df3786998cc2eeeab6c3e5a9446eb3d30edce584
GB-Navarro <gbnavarro@usp.br>
2022-09-16 21:46:41 -0300

    feat: created the getManyById function at the testsRepository

--- src/repositories/testsRepository.ts
+++ src/repositories/testsRepository.ts
@@ -1,16 +1,51 @@
 import { tests as Tests } from "@prisma/client";
 import { client } from "../dbStrategy/postgres.js";
 
-async function insert(data: Omit<Tests, "id">){
+async function insert(data: Omit<Tests, "id">) {
 
     const result = await client.tests.create({
         data: data
     })
 }
 
+async function getManyById(disciplineId: number) {
+    const result = await client.tests.findMany({
+        where: {
+            id: disciplineId
+        },
+        include: {
+            categories: {
+                select: {
+                    name: true
+                }
+            },
+            teacherDiscipline: {
+                include: {
+                    teacher: {
+                        select: {
+                            name: true
+                        }
+                    },
+                    disciplines: {
+                        select: {
+                            name: true,
+                            terms: {
+                                select: {
+                                    number: true
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
 const testsRepository = {
 
-    insert
+    insert,
+    getManyById
 }
 
 export default testsRepository;
