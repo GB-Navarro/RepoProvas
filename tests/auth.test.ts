647866276f232352e7127a8b78637ee60af2f43f
GB-Navarro <gbnavarro@usp.br>
2022-09-17 10:53:56 -0300

    feat: created the initial structure of auth routes tests

---
+++ tests/auth.test.ts
@@ -0,0 +1,8 @@
+describe("Teste das rotas de autenticação", () => {
+    it("Trying to create a user with wrong data format", () => {
+
+    })
+    it("Trying to create a user with wrong data format", () => {
+
+    })
+})
