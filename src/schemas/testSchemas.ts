68384f40df5dd72ca0826962224156e04a00a64c
GB-Navarro <gbnavarro@usp.br>
2022-09-17 08:52:33 -0300

    fix: fixed a atribbute of insert schema at testSchemas

--- src/schemas/testSchemas.ts
+++ src/schemas/testSchemas.ts
@@ -4,7 +4,7 @@ import Joi from "joi";
 
 const insert = Joi.object<ITestData>({
     name: Joi.string().min(4).required(),
-    pdfUrl: Joi.string().min(6).uri(),
+    pdfUrl: Joi.string().min(6).uri().required(),
     category: Joi.string().min(2).required(),
     discipline: Joi.string().min(3).required(),
     teacher: Joi.string().min(3).required(),
