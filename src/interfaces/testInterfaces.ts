46adffe4b793ca3779b2d78f7e72a84c9a4d654f
GB-Navarro <gbnavarro@usp.br>
2022-09-17 07:11:46 -0300

    feat: created the IUnformatedTestsObtainedByTermAndDiscipline interface

--- src/interfaces/testInterfaces.ts
+++ src/interfaces/testInterfaces.ts
@@ -1,9 +1,26 @@
-import { tests } from "@prisma/client";
-
 export type ITestData = {
     name: string,
     pdfUrl: string,
     category: string,
     discipline: string,
     teacher: string
+}
+
+export type IUnformatedTestsObtainedByTermAndDiscipline = {
+    name: string,
+    pdfUrl: string,
+    categories:{
+        name: string
+    },
+    teacherDiscipline:{
+        teacher:{
+            name: string
+        },
+        disciplines:{
+            name: string,
+            terms:{
+                number: number
+            }
+        }
+    }
 }
