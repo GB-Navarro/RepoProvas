b0b08bfffdc81234cb0c13b3c355d71e05cb1195
GB-Navarro <gbnavarro@usp.br>
2022-09-18 06:03:02 -0300

    feat: added a db connection to tests files and upgraded the test script at package.json

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -1,16 +1,19 @@
+import { client } from "../src/dbStrategy/postgres";
+
 import app from "../src/app";
 import supertest from "supertest";
+
 
 describe("Teste das rotas de autenticação", () => {
     it("Trying to create a user with wrong data format", async () => {
         const body = {
-
+            //Adicionar um body fake com a formatação errada
         }
 
         const result = await supertest(app).post("/sign-up").send(body);
         const status = result.status
 
-        expect(status).toEqual(404);
+        expect(status).toEqual(422);
     })
     /*it("Trying to create a user with wrong data format", () => {
 
@@ -18,4 +21,8 @@ describe("Teste das rotas de autenticação", () => {
     /*it("Conflict case", () => {
 
     })*/
+
+    afterAll(async () => {
+        await client.$disconnect();
+    })
 })
