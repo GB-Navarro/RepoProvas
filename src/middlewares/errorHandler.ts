d57635f4b148eea8e916aedbbf24e75ce88e057c
GB-Navarro <gbnavarro@usp.br>
2022-09-19 08:30:33 -0300

    feat: added some errors code

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -2,8 +2,11 @@ import { Request, Response, NextFunction } from "express";
 
 export default async function errorHandler(error: any, req: Request, res: Response, next: NextFunction){
 
-    if(error.code === "error_thisEmailIsNotRegistered" || error.code === "error_wrongPassword" || error.code === "error_notReceivedAToken"){
+    if(error.code === "error_thisEmailIsNotRegistered" || error.code === "error_wrongPassword" || error.code === "error_notReceivedAToken" || error.code === "error_invalidToken"){
         return res.status(401).send(error.message);
+    }
+    if(error.code === "error_thisTeacherDoesNotTeachThisDiscipline" || error.code === "This category does not exist!" || error.code === "error_thisTeacherDoesNotExist" || error.code === "error_thisDisciplineDoesNotExist"){
+        return res.status(404).send(error.message);
     }
     if(error.code === "error_emailAlreadyInUse"){
         return res.status(409).send(error.message);
