0268ee5b3edef9d88ff6b5c03927a90b1d2372ca
GB-Navarro <gbnavarro@usp.br>
2022-09-19 04:36:43 -0300

    feat: added a test who try to create a exam without the discipline attribute

--- tests/exam.test.ts
+++ tests/exam.test.ts
@@ -54,9 +54,18 @@ describe("/tests/insert", () => {
 
         expect(status).toEqual(422);
     })
-    /*it("Trying to create a exam without the discipline attribute", async () => {
+    it("Trying to create a exam without the discipline attribute", async () => {
 
-    })*/
+        const body = examFactory.createExamWithValidData();
+        const token = await utils.getToken();
+
+        delete body.discipline;
+
+        const result = await agent.post("/tests/insert").set('Authorization', token).send(body);
+        const status = result.status;
+
+        expect(status).toEqual(422);
+    })
     /*it("Trying to create a exam without the teacher attribute", async () => {
 
     })*/
