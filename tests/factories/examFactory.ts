5da3b96e4cb17aeae3c0f834415b844ca8669eb0
GB-Navarro <gbnavarro@usp.br>
2022-09-19 08:36:32 -0300

    feat: added some constats and conditions to ensure the validate of the data

--- tests/factories/examFactory.ts
+++ tests/factories/examFactory.ts
@@ -15,11 +15,28 @@ function generateExamData(name: string, pdfUrl: string, category: string, discip
 
 function createExamWithValidData() {
 
-    const name = faker.lorem.word();
-    const pdfUrl = faker.internet.url();
-    const category = faker.lorem.word();
-    const discipline = faker.lorem.word();
-    const teacher = faker.lorem.word();
+    const nameLength: number = Math.floor(Math.random() * 11) + 4;
+    const categoryNumber: number = Math.floor(Math.random() * 3);
+    const teacherNumber: number = Math.floor(Math.random() * 2);
+
+    const categories = ['Projeto', 'Prática', 'Recuperação'];
+    const teachers = ['Diego Pinho', 'Bruna Hamori'];
+    const disciplines = ["HTML e CSS","JavaScript","React", "Humildade","Planejamento","Autoconfiança"];
+
+    const name: string = faker.lorem.word(nameLength);
+    const pdfUrl: string = faker.internet.url();
+    const category: string = categories[categoryNumber];
+    const teacher: string = teachers[teacherNumber];
+    
+    let discipline:string;
+
+    if(teachers[teacherNumber] === 'Diego Pinho'){
+        const disciplineNumber: number = Math.floor(Math.random() * 3);
+        discipline = disciplines[disciplineNumber];
+    }else{
+        const disciplineNumber: number = Math.floor(Math.random() * 3) + 3;
+        discipline = disciplines[disciplineNumber];
+    }
 
     const examData = generateExamData(name, pdfUrl, category, discipline, teacher);
 
