a65000a3f9b1bb4b0e9537e62f01b511a42c6959
GB-Navarro <gbnavarro@usp.br>
2022-09-19 08:32:28 -0300

    refactor: the insert function has been refactored and some throws to catch errors have been added to it

--- src/services/testServices.ts
+++ src/services/testServices.ts
@@ -9,15 +9,67 @@ import teachersDisciplinesRepository from "../repositories/teachersDisciplinesRe
 
 import testUtils from "../utils/testUtils";
 
+async function getCategoryIdOrFail(categoryName: string){
+
+    const result = await categoriesRepository.getIdByName(categoryName);
+
+    if(result === null){
+        throw { code: "error_thisCategoryDoesNotExist", message: "This category does not exist!" };
+    }
+
+    const { id: categoryId }: { id: number } = result;
+
+    return categoryId
+}
+
+async function getTeacherIdOrFail(teacherName: string){
+
+    const result = await teachersRepository.getIdByName(teacherName);
+
+    if(result === null){
+        throw { code: "error_thisTeacherDoesNotExist", message: "This teacher does not exist!" };
+    }
+
+    const { id: teacherId }: { id: number } = result;
+
+    return teacherId
+}
+
+async function getDisciplineIdOrFail(discipline: string){
+
+    const result = await disciplinesRepository.getIdByName(discipline);
+
+    if(result === null){
+        throw { code: "error_thisDisciplineDoesNotExist", message: "This discipline does not exist!" };
+    }
+
+    const { id: disciplineId }: { id: number } =  result;
+
+    return disciplineId
+}
+
+async function getTeacherDisciplineIdOrFail(teacherId: number, disciplineId: number){
+
+    const result = await teachersDisciplinesRepository.getIdByTeacherIdAndDisciplineId(teacherId, disciplineId);
+    
+    if(result === null){
+        throw { code: "error_thisTeacherDoesNotTeachThisDiscipline", message: "This teacher does not teach this discipline!" };
+    }
+
+    const { id: teacherDisciplineId }: { id: number } = result
+
+    return teacherDisciplineId;
+}
+
 async function insert(data: ITestData) {
 
-    const { name, pdfUrl, category: categoryName, discipline, teacher }: ITestData = data;
+    const { name, pdfUrl, category: categoryName, discipline, teacher: teacherName }: ITestData = data;
 
-    const { id: categoryId }: { id: number } = await categoriesRepository.getIdByName(categoryName);
-    const { id: teacherId }: { id: number } = await teachersRepository.getIdByName(teacher);
-    const { id: disciplineId }: { id: number } = await disciplinesRepository.getIdByName(discipline);
-    const { id: teacherDisciplineId }: { id: number } = await teachersDisciplinesRepository.getIdByTeacherIdAndDisciplineId(teacherId, disciplineId);
-
+    const categoryId = await getCategoryIdOrFail(categoryName);
+    const teacherId = await getTeacherIdOrFail(teacherName);
+    const disciplineId = await getDisciplineIdOrFail(discipline);
+    const teacherDisciplineId = await getTeacherDisciplineIdOrFail(teacherId, disciplineId);
+    
     const insertData: Omit<Tests, "id"> = testUtils.generateInsertData(name, pdfUrl, categoryId, teacherDisciplineId);
 
     await testsRepository.insert(insertData);
