60201a63133fa64958e8e12b8d0aa0de32e97702
GB-Navarro <gbnavarro@usp.br>
2022-09-19 03:22:41 -0300

    feat: created a test to try to create a exam without the authorization token

--- tests/exam.test.ts
+++ tests/exam.test.ts
@@ -2,15 +2,23 @@ import { client } from "../src/dbStrategy/postgres";
 
 import app from "../src/app";
 import supertest from "supertest";
+import utils from "./testUtils/utils";
 import authFactory from "./factories/authFactory";
+import examFactory from "./factories/examFactory";
 
 describe("/tests/insert", () => {
 
     const agent = supertest(app);
 
-    /*it("Trying to create a exam without the authorization token", async () => {
+    it("Trying to create a exam without the authorization token", async () => {
 
-    })*/
+        const body = examFactory.createExamWithValidData();
+
+        const result = await agent.post("/tests/insert").send(body);
+        const status = result.status;
+
+        expect(status).toEqual(401);
+    })
     /*it("Trying to create a exam without the name attribute", async () => {
 
     })*/
