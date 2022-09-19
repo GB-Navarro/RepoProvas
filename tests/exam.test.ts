bdf3a4bafe2dbbca0fb0ff6f9c94a1c2dac234a7
GB-Navarro <gbnavarro@usp.br>
2022-09-19 04:38:31 -0300

    feat: added a test who try to create a exam without the teacher attribute

--- tests/exam.test.ts
+++ tests/exam.test.ts
@@ -66,9 +66,18 @@ describe("/tests/insert", () => {
 
         expect(status).toEqual(422);
     })
-    /*it("Trying to create a exam without the teacher attribute", async () => {
+    it("Trying to create a exam without the teacher attribute", async () => {
 
-    })*/
+        const body = examFactory.createExamWithValidData();
+        const token = await utils.getToken();
+
+        delete body.teacher;
+
+        const result = await agent.post("/tests/insert").set('Authorization', token).send(body);
+        const status = result.status;
+
+        expect(status).toEqual(422);
+    })
     /*it("Sucess", async () => {
 
     })*/
